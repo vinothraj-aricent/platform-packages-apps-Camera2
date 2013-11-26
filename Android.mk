c126aa60085146effda83e3a889ecdf5d6c14d01
Fang Hui <b31070@freescale.com>
2013-11-26 16:52:49 +0800

    ENGR00308369 enable camera function for 6sx sdb board.
    when there are GPU3D and VPU module, use new Camera2.
    update to pass build for android 4.4.
    evk_6sl not support 3D, so remote it from build for evk_6sl.
    
    This commit merge with below commit:
    60c0f71 ENGR00215174 update to pass build for android 4.4
    
    Change-Id: I85ae0ed57d5294cb425e482c1a28e9029a1244cd
    Signed-off-by: Fang Hui <b31070@freescale.com>
    Signed-off-by: Xiaowen Liu <b37945@freescale.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/477
    Reviewed-by: guoyin.chen <guoyin.chen@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/879
    Reviewed-by: Xiaowen Liu <xiaowen.liu@nxp.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,5 @@
+ifeq ($(HAVE_FSL_IMX_GPU3D),true)
+ifeq ($(BOARD_HAVE_VPU),true)
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -40,3 +42,5 @@ LOCAL_JNI_SHARED_LIBRARIES := libjni_tinyplanet libjni_jpegutil
 include $(BUILD_PACKAGE)
 
 include $(call all-makefiles-under, $(LOCAL_PATH))
+endif
+endif
